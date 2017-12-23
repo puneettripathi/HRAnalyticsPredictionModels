e1045341bc520da0553828175422e682afbd1d64
Puneet Tripathi <31759593+puneettripathi@users.noreply.github.com>
2017-12-23 20:18:46 +0530

    Update HRAnalyticsLogisticRegression.R

--- HRAnalyticsLogisticRegression.R
+++ HRAnalyticsLogisticRegression.R
@@ -20,10 +20,6 @@ in_time <- read.csv("in_time.csv")
 manager_survey_data <- read.csv("manager_survey_data.csv")
 out_time <- read.csv("out_time.csv")
 
-# Fixing NA in intime and outtime
-intime <- in_time[,! apply( in_time , 2 , function(x) all(is.na(x)) )]
-outtime <- out_time[,! apply( out_time , 2 , function(x) all(is.na(x)) )]
-
 # Checking summary statistics on dataframes
 summary(employee_survey_data)
 summary(general_data)
@@ -43,8 +39,8 @@ str(out_time)
 length(unique(tolower(employee_survey_data$EmployeeID)))    # 4410, confirming EmployeeID is key 
 length(unique(tolower(general_data$EmployeeID))) # 4410, confirming EmployeeID is key
 length(unique(tolower(manager_survey_data$EmployeeID))) # 4410, confirming EmployeeID is key
-length(unique(tolower(outtime$X))) # 4410, confirming X(EmployeeID) is key
-length(unique(tolower(intime$X))) # 4410, confirming X(EmployeeID) is key
+length(unique(tolower(out_time$X))) # 4410, confirming X(EmployeeID) is key
+length(unique(tolower(in_time$X))) # 4410, confirming X(EmployeeID) is key
 
 # Checking if there is difference in EmployeeId 
 setdiff(employee_survey_data$EmployeeID,general_data$EmployeeID) # Identical EmployeeID across these datasets
@@ -112,7 +108,7 @@ emp_avghours_pw <- aggregate(value~EmployeeID, temp_df, sum)
 emp_avghours_pw$value <- emp_avghours_pw$value/52
 
 emp_extraOffs <- aggregate(value~EmployeeID, temp_df[temp_df$value==0,], length)
-colnames(emp_avghours_pw)[names(emp_avghours_pw) == "value"] = "avg_wrokhours_per_week"
+colnames(emp_avghours_pw)[names(emp_avghours_pw) == "value"] = "avg_workhours_per_week"
 colnames(emp_extraOffs)[names(emp_extraOffs) == "value"] = "Num_of_days_off"
 metrics_emptime <- cbind(emp_avghours_pw,emp_extraOffs$Num_of_days_off)
 
@@ -121,49 +117,67 @@ metrics_emptime <- cbind(emp_avghours_pw,emp_extraOffs$Num_of_days_off)
 employeeHr<- merge(employeeHr,metrics_emptime, by="EmployeeID", all = F)
 
 #Create deried metric - if person works overtime or not
-employeeHr$overtime = ifelse(employeeHr$avg_wrokhours_per_week/40> 1, 1, 0) 
+employeeHr$overtime = ifelse(employeeHr$avg_workhours_per_week/40> 1, 1, 0) 
 colnames(employeeHr)[names(employeeHr) == "emp_extraOffs$Num_of_days_off"] = "Num_of_days_off"
 
 # Create AgeGroups
 unique(employeeHr$Age)
-# AgeGroups 20_30, 30_40, 40_50, ovr_50
-employeeHr$AgeGroup <- ifelse(20 < employeeHr$Age & employeeHr$Age <= 30 , "20_30",
-                                ifelse(30 < employeeHr$Age & employeeHr$Age <= 40 , "31_40",
-                                       ifelse(40 < employeeHr$Age & employeeHr$Age <= 50 , "41_50", "over_50")
-                                ))
-
+# Create AgeGroups
+sort(unique(employeeHr$Age))
+# AgeGroups Under_30, 31_40, 41_50, ovr_50
+employeeHr$AgeGroup <- ifelse(employeeHr$Age <= 30, "Under_30",
+                              ifelse(employeeHr$Age <= 40, "31_40",
+                                     ifelse(employeeHr$Age <= 50, "41_50", "over_50")
+                              ))
+summary(factor(employeeHr$AgeGroup))
 
 # Create groups for NumCompaniesWorked
+sort(unique(employeeHr$NumCompaniesWorked))
 employeeHr$CmpWorkGroup = ifelse(employeeHr$NumCompaniesWorked <= 2 , "0_2",
-                                 ifelse(2 < employeeHr$NumCompaniesWorked & employeeHr$NumCompaniesWorked <= 5 , "3_5",
-                                        ifelse(5 < employeeHr$NumCompaniesWorked & employeeHr$NumCompaniesWorked <= 8 , "6_8", 
-                                               "over_8")
+                                 ifelse(employeeHr$NumCompaniesWorked <= 5 , "3_5",
+                                        ifelse(employeeHr$NumCompaniesWorked <= 8 , "6_8", "over_8")
                                  ))
+summary(factor(employeeHr$CmpWorkGroup))
 
 # Creating segments of YearsSinceLastPromotion
-
-employeeHr$LastPromotionSeg = ifelse(employeeHr$YearsSinceLastPromotion <= 2 , "0_2",
-                                 ifelse(2 < employeeHr$YearsSinceLastPromotion & employeeHr$YearsSinceLastPromotion <= 5 , "3_5",
-                                        "over_5"))
+sort(unique(employeeHr$YearsSinceLastPromotion))
+employeeHr$LastPromotionSeg = ifelse(employeeHr$YearsSinceLastPromotion <= 3 , "0_2",
+                                     ifelse(employeeHr$YearsSinceLastPromotion <= 5, "3_5",
+                                            ifelse(employeeHr$YearsSinceLastPromotion <= 9 , "6_9", "over_9"
+                                            )))
+summary(factor(employeeHr$LastPromotionSeg))
 
 # Create groups for YearsWithCurrManager
+sort(unique(employeeHr$YearsWithCurrManager))
 employeeHr$CurrentMgrTimeSeg = ifelse(employeeHr$YearsWithCurrManager <= 2 , "0_2",
-                                 ifelse(2 < employeeHr$YearsWithCurrManager & employeeHr$YearsWithCurrManager <= 5 , "3_5",
-                                        ifelse(5 < employeeHr$YearsWithCurrManager & employeeHr$YearsWithCurrManager <= 8 , "6_8", 
-                                               "over_8")
-                                 ))
+                                      ifelse(employeeHr$YearsWithCurrManager <= 5 , "3_5",
+                                             ifelse(employeeHr$YearsWithCurrManager <= 8 , "6_8", 
+                                                    "over_8")
+                                      ))
+summary(factor(employeeHr$CurrentMgrTimeSeg))
 
 # Create groups for YearsAtCompany
+sort(unique(employeeHr$YearsAtCompany))
 employeeHr$CompanyYearSeg = ifelse(employeeHr$YearsAtCompany <= 2 , "0_2",
-                                      ifelse(2 < employeeHr$YearsAtCompany & employeeHr$YearsAtCompany <= 5 , "3_5",
-                                             ifelse(5 < employeeHr$YearsAtCompany & employeeHr$YearsAtCompany <= 8 , "6_8", 
-                                                    ifelse(8 < employeeHr$YearsAtCompany & employeeHr$YearsAtCompany <= 11 , "8_11",
-                                                    "over_11"))))
+                                   ifelse(employeeHr$YearsAtCompany <= 5 , "3_5",
+                                          ifelse(employeeHr$YearsAtCompany <= 8 , "6_8", 
+                                                 ifelse(employeeHr$YearsAtCompany <= 11 , "8_11",
+                                                        "over_11"))))
+summary(factor(employeeHr$CompanyYearSeg))
+
+# Create groups for TotalWorkingYears
+sort(unique(employeeHr$TotalWorkingYears))
+employeeHr$WorkYearSeg = ifelse(employeeHr$TotalWorkingYears <= 3 , "0_3",
+                                   ifelse(3 < employeeHr$TotalWorkingYears & employeeHr$TotalWorkingYears <= 7 , "4_7",
+                                          ifelse(7 < employeeHr$TotalWorkingYears & employeeHr$TotalWorkingYears <= 11 , "8_11", 
+                                                 ifelse(11 < employeeHr$TotalWorkingYears & employeeHr$TotalWorkingYears <= 15 , "12_15",
+                                                        ifelse(15 < employeeHr$TotalWorkingYears & employeeHr$TotalWorkingYears <= 20 , "16_20", "over_21")))))
+summary(factor(employeeHr$WorkYearSeg))
 
 # Master dataset
 View(employeeHr) 
 length(names(employeeHr))
-#32 Columns
+#38 Columns
 
 ##### Start of EDA #####
 # Create theme for bar plot
@@ -227,10 +241,10 @@ plot_grid(ggplot(employeeHr, aes(x=as.factor(YearsWithCurrManager),fill=Attritio
 
 plot_grid(ggplot(employeeHr, aes(x=Attrition,y=DistanceFromHome)) + geom_boxplot(),
           ggplot(employeeHr, aes(x=DistanceFromHome)) + geom_histogram(bins = 15),
+          ggplot(employeeHr, aes(x=as.factor(employeeHr$DistanceFromHome),fill=Attrition))+ geom_bar()+bar_theme1,
           align="h")
 # most people have Distance less than 10Km
-# Data is right skewed and people resign have larger spread meaning 
-# they travel more even though median in both cases 
+# When we look absolute numbers we find that most people leaving job is living within 10KM
 
 plot_grid(ggplot(employeeHr, aes(x=Attrition,y=Age)) + geom_boxplot(),
           ggplot(employeeHr, aes(x=Age)) + geom_histogram(bins=25),
@@ -247,9 +261,13 @@ plot_grid(ggplot(employeeHr, aes(x=Attrition,y=PercentSalaryHike)) + geom_boxplo
 # mostly people get 10 to 15% salary hike
 
 plot_grid(ggplot(employeeHr, aes(x=Attrition,y=TotalWorkingYears)) + geom_boxplot(),
-          ggplot(employeeHr, aes(x=TotalWorkingYears)) + geom_histogram(bins = 40))
+          ggplot(employeeHr, aes(x=TotalWorkingYears)) + geom_histogram(bins = 40),
+          ggplot(employeeHr, aes(x=as.factor(TotalWorkingYears),fill=Attrition))+ geom_bar()+bar_theme1,
+          ggplot(employeeHr, aes(x=as.factor(WorkYearSeg),fill=Attrition))+ geom_bar()+bar_theme1)
 # People leaving company have median 7 years of experience and have relatively lower overall experience
 # This might mean as people get more experienced they tend to stay at same company for longer time
+# From segmented Bar plot, we can veerify above hypothesis, that people with lower work exp tend to leave jobs more
+# We will use WorkYearSeg for further analysis and drop TotalWorkingYears
 
 plot_grid(ggplot(employeeHr, aes(x=as.factor(YearsSinceLastPromotion),fill=Attrition))+ geom_bar()+bar_theme1,
           ggplot(employeeHr, aes(x=as.factor(LastPromotionSeg),fill=Attrition))+ geom_bar()+bar_theme1 )
@@ -257,8 +275,8 @@ plot_grid(ggplot(employeeHr, aes(x=as.factor(YearsSinceLastPromotion),fill=Attri
 # that might make sense as well because that means reaching higher salary/ higher grade jump in relatively lower time span
 # The segmentated vaiable captures the data variablitily better and smoother, we will use that
 
-plot_grid(ggplot(employeeHr, aes(x=Attrition,y=avg_wrokhours_per_week)) + geom_boxplot(),
-          ggplot(employeeHr, aes(x=avg_wrokhours_per_week)) + geom_histogram(bins = 40))
+plot_grid(ggplot(employeeHr, aes(x=Attrition,y=avg_workhours_per_week)) + geom_boxplot(),
+          ggplot(employeeHr, aes(x=avg_workhours_per_week)) + geom_histogram(bins = 40))
 # People resigning are those who are generally overworked
 # Mostlypeople are working less than 35 hours & very few over 50
 
@@ -294,9 +312,12 @@ plot_grid(ggplot(employeeHr, aes(x=Attrition,y=MonthlyIncome)) + geom_boxplot(),
 
 ##### Drop variable with just one value #####
 employeeHr <- Filter(function(x)(length(unique(x))>1), employeeHr)
-employeeHr <- employeeHr[, !(colnames(employeeHr) %in% c('Age','NumCompaniesWorked', "YearsSinceLastPromotion", "YearsWithCurrManager", "YearsAtCompany"))]
+
+# Drop Variables which we have created Segmented Variable for
+employeeHr <- employeeHr[, !(colnames(employeeHr) %in% c('Age','NumCompaniesWorked', "YearsSinceLastPromotion", "YearsWithCurrManager", "YearsAtCompany", "incomegroup"))]
+
 ##### Outlier treatment for contiuous variables #####
-list_of_num_cols <- c("DistanceFromHome", "MonthlyIncome", "PercentSalaryHike", "TotalWorkingYears", "avg_wrokhours_per_week", "Num_of_days_off")
+list_of_num_cols <- c("DistanceFromHome", "MonthlyIncome", "PercentSalaryHike", "avg_workhours_per_week", "Num_of_days_off")
 # Total unique values in each column 
 apply(employeeHr[,list_of_num_cols], 2, function(x)length(unique(x)))
 
@@ -307,25 +328,64 @@ xx = sapply(employeeHr[,list_of_num_cols],
        function(x) quantile(x,seq(0,1,.01),na.rm = T))
 
 # variables that need outlier treatment
-# MonthlyIncome, TotalWorkingYears, YearsWithCurrManager, avg_wrokhours_per_week
+# MonthlyIncome, avg_workhours_per_week
 
 ## Imputing Outliers with median value
 out_pos_inc <- which(employeeHr$MonthlyIncome %in% boxplot.stats(employeeHr$MonthlyIncome)$out)
 employeeHr$MonthlyIncome[out_pos_inc] <- NA
 employeeHr$MonthlyIncome[is.na(employeeHr$MonthlyIncome)] <- median(employeeHr$MonthlyIncome, na.rm = TRUE)
 
-out_pos_twy <- which(employeeHr$TotalWorkingYears %in% boxplot.stats(employeeHr$TotalWorkingYears)$out)
-employeeHr$TotalWorkingYears[out_pos_twy] <- NA
-employeeHr$TotalWorkingYears[is.na(employeeHr$TotalWorkingYears)] <- median(employeeHr$TotalWorkingYears, na.rm = TRUE)
-
-out_pos_awh <- which(employeeHr$avg_wrokhours_per_week %in% boxplot.stats(employeeHr$avg_wrokhours_per_week)$out)
-employeeHr$avg_wrokhours_per_week[out_pos_awh] <- NA
-employeeHr$avg_wrokhours_per_week[is.na(employeeHr$avg_wrokhours_per_week)] <- median(employeeHr$avg_wrokhours_per_week, na.rm = TRUE)
+out_pos_awh <- which(employeeHr$avg_workhours_per_week %in% boxplot.stats(employeeHr$avg_workhours_per_week)$out)
+employeeHr$avg_workhours_per_week[out_pos_awh] <- NA
+employeeHr$avg_workhours_per_week[is.na(employeeHr$avg_workhours_per_week)] <- median(employeeHr$avg_workhours_per_week, na.rm = TRUE)
 
 ##### Dummy Variable Creation #####
-factor_Variables <- c("CompanyYearSeg", "CurrentMgrTimeSeg", "LastPromotionSeg", "TrainingTimesLastYear", "AgeGroup", "CmpWorkGroup", "EnvironmentSatisfaction", "JobSatisfaction", "WorkLifeBalance", "BusinessTravel", "Department", "Education", "EducationField", "Gender", "JobLevel", "JobRole", "MaritalStatus", "StockOptionLevel", "JobInvolvement", "PerformanceRating", "overtime", "incomegroup" )
+factor_Variables <- c("WorkYearSeg", "CompanyYearSeg", "CurrentMgrTimeSeg", "LastPromotionSeg", "TrainingTimesLastYear", "AgeGroup", "CmpWorkGroup", "EnvironmentSatisfaction", "JobSatisfaction", "WorkLifeBalance", "BusinessTravel", "Department", "Education", "EducationField", "Gender", "JobLevel", "JobRole", "MaritalStatus", "StockOptionLevel", "JobInvolvement", "PerformanceRating", "overtime" )
 fact_table <- employeeHr[,factor_Variables]
-non_fact_table <- employeeHr[,!colnames(employeeHr) %in% factor_Variables]
+fact_table$TrainingTimesLastYear <- as.factor(fact_table$TrainingTimesLastYear)
+fact_table$EnvironmentSatisfaction <- as.factor(fact_table$EnvironmentSatisfaction)
+fact_table$JobSatisfaction <- as.factor(fact_table$JobSatisfaction)
+fact_table$WorkLifeBalance <- as.factor(fact_table$WorkLifeBalance)
+fact_table$Education <- as.factor(fact_table$Education)
+fact_table$JobLevel <- as.factor(fact_table$JobLevel)
+fact_table$StockOptionLevel <- as.factor(fact_table$StockOptionLevel)
+fact_table$JobInvolvement <- as.factor(fact_table$JobInvolvement)
+fact_table$PerformanceRating <- as.factor(fact_table$PerformanceRating)
+fact_table$overtime <- as.factor(fact_table$overtime)
 
+
+# Create Dummy Variables
 dummies<- data.frame(sapply(fact_table, 
                             function(x) data.frame(model.matrix(~x-1,data =fact_table))[,-1]))
+##### Treating continuous Variables #####
+non_fact_table <- employeeHr[,!colnames(employeeHr) %in% factor_Variables]
+emp_Attr <- non_fact_table[, 1:2]
+cont_var_df <- data.frame(sapply(non_fact_table[, 3:8], 
+                                 function(x) scale(x)))
+
+# Creating Final dataframe for building model
+final_df <- cbind(emp_Attr,cont_var_df, dummies)
+final_df$Attrition <- ifelse(as.character(final_df$Attrition) == "Yes",1,0)
+
+##### Seeing Correlation Matrix #####
+cormatt = cor(final_df[2:80])
+cormatt
+
+##### Splitting data in train and test #####
+set.seed(100)
+trainindices= sample(1:nrow(final_df), 0.7*nrow(final_df))
+train = final_df[trainindices,]
+test = final_df[-trainindices,]
+
+
+##### Start building Logistic Regression Model here #####
+
+#Initial model
+model_1 = glm(Attrition ~ ., data = final_df, family = "binomial")
+summary(model_1) #AIC 2954.2 coeff : nullDev 3895.7, resDev 2794.2
+
+# Stepwise selection
+model_2<- stepAIC(model_1, direction="both")
+
+summary(model_2)
+
